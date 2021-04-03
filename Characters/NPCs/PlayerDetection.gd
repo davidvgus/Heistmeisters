b1470db09e2f84293dd5ed38854cdfa48049245c
David Gustafson <git@radicalskeptic.net>
2021-04-02 17:29:38 -0700

    adds line of site code

--- Characters/NPCs/PlayerDetection.gd
+++ Characters/NPCs/PlayerDetection.gd
@@ -11,7 +11,7 @@ func _ready():
     Player = get_node("/root").find_node("Player", true, false)
 
 func _process(delta):
-    if Player_in_FOV():
+    if Player_in_FOV() and Player_in_LOS():
         $Torch.color = RED
     else:
         $Torch.color = WHITE
@@ -22,6 +22,21 @@ func Player_in_FOV():
     var direction_to_Player = (Player.position - global_position).normalized()
 
     if abs(direction_to_Player.angle_to(npc_facing_direction)) < deg2rad(FOV_TOLERANCE):
+        return true
+    else:
+        return false
+
+func Player_in_LOS():
+    var space = get_world_2d().direct_space_state
+    var LOS_obstacle = space.intersect_ray(global_position, Player.global_position, 
+    [self], collision_mask)
+
+    if not LOS_obstacle:
+        return false
+
+    var distance_to_player = Player.global_position.distance_to(global_position)
+
+    if LOS_obstacle.collider == Player:
         return true
     else:
         return false
